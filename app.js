c7f405181e71dcda967a087a827ec0bbe9247059
Muhammad-Ali-Asghar-Rizwan <164878048+Muhammad-Ali-Asghar-Rizwan@users.noreply.github.com>
2024-11-25 14:18:56 +0500

    first commit

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 var changeColorBtn = document.getElementById('changeColor');
 var colorCodeElement = document.getElementById('colorCode');
-// Function to generate a random hex color
-function genrateRandomColor() {
-    var letters = '0123456789ABCDEF';
+var genrateRandomColor = function () {
+    var lettera = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+        color += lettera[Math.floor(Math.random() * 16)];
     }
     return color;
-}
+};
 changeColorBtn === null || changeColorBtn === void 0 ? void 0 : changeColorBtn.addEventListener("click", function () {
-    var newColor = genrateRandomColor();
-    document.body.style.backgroundColor = newColor;
+    var newcolor = genrateRandomColor();
+    document.body.style.backgroundColor = newcolor;
     if (colorCodeElement) {
-        colorCodeElement.textContent = newColor;
+        colorCodeElement.textContent = newcolor;
     }
 });
--- app.ts
+++ app.ts
@@ -1,30 +1,28 @@
 const changeColorBtn = document.getElementById('changeColor')
 
-
 const colorCodeElement = document.getElementById('colorCode')
 
 
 
-// Function to generate a random hex color
 
-function genrateRandomColor(){
-    const letters = '0123456789ABCDEF';
+const genrateRandomColor  = ():string => {
+    const lettera = '0123456789ABCDEF';
 
     let color = '#'
 
-    for (let i = 0; i< 6; i++) {
-     color += letters[Math.floor(Math.random() * 16)]  
+    for (let i = 0; i < 6; i++) {
+       color += lettera[Math.floor(Math.random()*16)]        
     }
     return color;
-    
 }
 
 
 changeColorBtn?.addEventListener("click", ()=>{
-    const newColor = genrateRandomColor();
-    document.body.style.backgroundColor = newColor
+    const newcolor = genrateRandomColor()
+    document.body.style.backgroundColor = newcolor
+
 
     if (colorCodeElement) {
-        colorCodeElement.textContent = newColor
-    }
+        colorCodeElement.textContent = newcolor
+    } 
 })
